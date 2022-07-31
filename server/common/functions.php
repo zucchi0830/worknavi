d6feb00dee1bca10c72ce5dae72e8c8dca7b5cdb
zucchi0830 <zucchi0830@gmail.com>
2022-07-31 17:06:11 +0900

    first commit

---
+++ server/common/functions.php
@@ -0,0 +1,26 @@
+<?php
+require_once __DIR__ . '/config.php';
+
+// 接続処理を行う関数
+function connect_db()
+{
+    try {
+        return new PDO(
+            DSN,
+            USER,
+            PASSWORD,
+            [PDO::ATTR_ERRMODE =>
+            PDO::ERRMODE_EXCEPTION]
+        );
+    } catch (PDOException $e) {
+        echo $e->getMessage();
+        exit;
+    }
+}
+
+// エスケープ処理を行う関数
+function h($str)
+{
+    // ENT_QUOTES: シングルクオートとダブルクオートを共に変換する。
+    return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+}
---
+++ server/index.php
@@ -0,0 +1,7 @@
+<?php
+
+// 関数ファイルを読み込む
+require_once __DIR__ . '/common/functions.php';
+
+// データベースに接続
+$dbh = connect_db();
